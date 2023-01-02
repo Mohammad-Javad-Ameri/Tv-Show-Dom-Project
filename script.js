522597ccfe8c7ef1112ccbc01f7b18fa63b5062b
Mohammad-Javad-Ameri <110919726+Mohammad-Javad-Ameri@users.noreply.github.com>
2023-01-02 13:45:57 +0330

    Final Edits
    Final Edits

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
 const getEpisode = async () => {
-    const response = await fetch("episodes.json");
+    const response = await fetch("https://api.tvmaze.com/shows/527/episodes");
     const data = await response.json();
 
     return data;
 }
 function res(get) {
-    console.log(get);
+
 }
 let episodes = 0;
 let season = 0;
 let episodeInfo = [];
 const form = document.querySelector("#searchForm");
+const select = document.querySelector("#selector");
+const found = document.querySelector(".found");
 form.addEventListener("input", async (e) => {
     const value = e.target.value.toLowerCase();
-    console.log(value);
-    episodeInfo.forEach(info => {
-        const isVisible = info.name.toLowerCase().includes(value) || info.summary.toLowerCase().includes(value);
-        info.element.classList.toggle("hide", !isVisible)
-    })
+    for (i = 0; i < episodeInfo.length; i++) {
 
+    }
+    episodeInfo.map((info, i) => {
+        if (info.name.toLowerCase().includes(value) || info.summary.toLowerCase().includes(value)) {
+            info.element.classList.remove("hide")
+        }
+        else {
+            info.element.classList.add("hide");
+        }
+    })
+    const Found = episodeInfo.filter((list => {
+        return (list.name.toLowerCase().includes(value) || list.summary.toLowerCase().includes(value));
+    }))
+    getEpisode(Found);
+
+    if (Found.length > 0) {
+        found.textContent = `${Found.length} Episodes Found`;
+    }
+    else {
+        found.textContent = "Episode not found";
+    }
+    if (e.target.value === " ") {
+        found.classList.add("hide");
+    }
+
+
+})
+select.addEventListener("click", (e) => {
+    const value = e.target.value;
+
+    episodeInfo.forEach(list => {
+        const isVisible = list.All.includes(value)
+        list.class.classList.toggle("hide", !isVisible)
+
+    })
 })
 getEpisode()
     .then((data) => {
         episodeInfo = data.map(Info => {
-
-
             const div = document.querySelector("div");
             const section = document.createElement("section");
             section.className = "card";
@@ -46,12 +76,17 @@ getEpisode()
             summary.innerHTML = Info.summary;
             section.append(img, name, episode, summary);
             div.append(section);
+            const option = document.createElement("option");
+            const nameAndEpisode = option.innerHTML = `${episode.innerText} - ${Info.name}`;
+            select.appendChild(option);
 
-            return { name: Info.name, summary: Info.summary, element: section }
+
+
+            return { name: Info.name, summary: Info.summary, element: section, All: nameAndEpisode, class: section }
         });
-        console.log(data)
+
     })
-    .catch(() => {
-        console.log(error);
+    .catch((error) => {
+        console.log("rejected", error);
     })
 
